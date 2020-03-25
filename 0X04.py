98d352fd3bfa4cf5c56a67af79542fa8c062bfc0
bryanbuiles <60362838+bryanbuiles@users.noreply.github.com>
2020-03-25 16:15:41 -0500

    Delete 0X04.py

--- 0X04.py
+++
@@ -1,9 +0,0 @@
-def modulo_C(z):
-    j = abs(z)
-    return j
-
-
-dato = float(input("Por favor, ingresa el real: "))
-modulo = float(input("Por favor, ingresa el imaginario: "))
-z = complex(dato, modulo)
-print("El modulo del numero complejo es: ", modulo_C(z))
