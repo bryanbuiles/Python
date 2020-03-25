3b85390f3ad7fc5c208b210c71fb0a9772763e4d
bryanbuiles <60362838+bryanbuiles@users.noreply.github.com>
2020-03-25 16:15:25 -0500

    Delete 0X07.py

--- 0X07.py
+++
@@ -1,8 +0,0 @@
-for carac in range(5000):
-    print("Yo voy a ganar la materia")
-print('\n')
-print("segunda manera \n")
-i = 0
-while i < 5000:
-    print("Yo voy a ganar la materia")
-    i = i + 1
